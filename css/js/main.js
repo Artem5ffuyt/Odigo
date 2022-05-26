3a73a2bf32670ba38641d178fdced551092f73a8
Artem Bachynskyi <97476036+Artem5ffuyt@users.noreply.github.com>
2022-05-26 22:29:09 +0300

    верстка

--- css/js/main.js
+++ css/js/main.js
@@ -8,3 +8,15 @@ window.onscroll = () => {
     }
 };
 }());
+
+(function() {
+    const burgerItem = document.querySelector('.burger');
+    const menu = document.querySelector('.header__nav');
+    const menuCloseItem = document.querySelector('.heder_nav-close');
+    burgerItem.addEventListener('click', () => {
+        menu.classList.add('header__nav_active');
+    });
+    menuCloseItem.addEventListener('clik',() => {
+        menu.classList.remove('header__nav_active');
+});
+}());
